4761623b1739cd831ebbc49ef2e4dc8acffe6618
ChenESia <cheneus@gmail.com>
2018-01-04 17:44:11 -0600

    init

--- serverWish.js
+++ serverWish.js
@@ -19,7 +19,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "m44t4236773T$",
+  password: "",
   database: "burgers_db"
 });
 
