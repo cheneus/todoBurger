2749f1dde87b2bd4ea57e34b5e38e8c724cb2d11
ChenESia <cheneus@gmail.com>
2018-01-04 17:40:26 -0600

    server.js update

--- serverWish.js
+++ serverWish.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -84,4 +84,6 @@ app.put("/update", (req, res) => {
   res.redirect(303, '/')
 })
 
-app.listen(port);
+app.listen(port function() {
+  console.log("App listening on PORT " + PORT);
+});
